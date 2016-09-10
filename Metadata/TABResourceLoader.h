b018d40c52a9466d89c9e7220fe5281b27c5b709
Luciano Marisi <luciano@techbrewers.com>
2016-09-10 15:54:39 +0100

    Initial commit

---
+++ Metadata/TABResourceLoader.h
@@ -0,0 +1,19 @@
+//
+//  TABResourceLoader.h
+//  TABResourceLoader
+//
+//  Created by Luciano Marisi on 10/09/2016.
+//  Copyright © 2016 Luciano Marisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TABResourceLoader.
+FOUNDATION_EXPORT double TABResourceLoaderVersionNumber;
+
+//! Project version string for TABResourceLoader.
+FOUNDATION_EXPORT const unsigned char TABResourceLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TABResourceLoader/PublicHeader.h>
+
+
