66fd592bb49ed2172cd024298b12b153eab8bd0c
Luciano Marisi <luciano@techbrewers.com>
2016-09-10 19:37:23 +0100

    Added convenience functions for transforming Result type

--- Sources/Concrete types/Result.swift
+++ Sources/Concrete types/Result.swift
@@ -13,8 +13,28 @@ public enum Result<T> {
   case Failure(ErrorType)
 }
 
+// http://www.cocoawithlove.com/blog/2016/08/21/result-types-part-one.html#conclusion-and-usage
 extension Result {
+  // Construct a `Result` from a Swift `throws` error handling function
+  public init(_ capturing: () throws -> T) {
+    do {
+      self = .Success(try capturing())
+    } catch {
+      self = .Failure(error)
+    }
+  }
+  
+  // Convert the `Result` back to typical Swift `throws` error handling
+  public func unwrap() throws -> T {
+    switch self {
+    case .Success(let v): return v
+    case .Failure(let e): throw e
+    }
+  }
+}
 
+extension Result {
+  
   /// Returns the success result if it exists, otherwise nil
   func successResult() -> T? {
     switch self {
@@ -24,7 +44,7 @@ extension Result {
       return nil
     }
   }
-
+  
   /// Returns the error if it exists, otherwise nil
   func error() -> ErrorType? {
     switch self {
