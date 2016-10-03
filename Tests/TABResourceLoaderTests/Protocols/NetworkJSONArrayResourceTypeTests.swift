3f35b34b1734a4906088791f1602b320154cd63c
Luciano Marisi <luciano@techbrewers.com>
2016-10-03 16:33:42 +0100

    Added tests for NetworkJSONArrayResourceType and NetworkJSONDictionaryResourceType extension

---
+++ Tests/TABResourceLoaderTests/Protocols/NetworkJSONArrayResourceTypeTests.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkJSONArrayResourceTypeTests.swift
+//  TABResourceLoader
+//
+//  Created by Luciano Marisi on 03/10/2016.
+//  Copyright © 2016 Luciano Marisi. All rights reserved.
+//
+
+import XCTest
+@testable import TABResourceLoader
+
+struct MockNetworkJSONArrayResourceType: NetworkJSONArrayResourceType {
+  typealias Model = String
+  let url: URL
+  
+  func modelFrom(jsonArray: [Any]) -> String? {
+    return ""
+  }
+  
+}
+
+class NetworkJSONArrayResourceTypeTests: XCTestCase {
+
+  let url = URL(string: "www.test.com")!
+
+  func test_correctDefaultValues() {
+    let resource = MockNetworkJSONArrayResourceType(url: url)
+    XCTAssertEqual(resource.HTTPRequestMethod, HTTPMethod.GET)
+    XCTAssertEqual(resource.HTTPHeaderFields!, ["Content-Type": "application/json"])
+    XCTAssertNil(resource.JSONBody)
+    XCTAssertNil(resource.queryItems)
+  }
+
+}
---
+++ Tests/TABResourceLoaderTests/Protocols/NetworkJSONDictionaryResourceTypeTests.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkJSONDictionaryResourceTypeTests.swift
+//  TABResourceLoader
+//
+//  Created by Luciano Marisi on 03/10/2016.
+//  Copyright © 2016 Luciano Marisi. All rights reserved.
+//
+
+import XCTest
+@testable import TABResourceLoader
+
+struct MockNetworkJSONDictionaryResourceType: NetworkJSONDictionaryResourceType {
+  typealias Model = String
+  let url: URL
+  
+  func modelFrom(jsonDictionary: [String : Any]) -> String? {
+    return ""
+  }
+  
+}
+
+class NetworkJSONDictionaryResourceTypeTests: XCTestCase {
+
+  let url = URL(string: "www.test.com")!
+
+  func test_correctDefaultValues() {
+    let resource = MockNetworkJSONDictionaryResourceType(url: url)
+    XCTAssertEqual(resource.HTTPRequestMethod, HTTPMethod.GET)
+    XCTAssertEqual(resource.HTTPHeaderFields!, ["Content-Type": "application/json"])
+    XCTAssertNil(resource.JSONBody)
+    XCTAssertNil(resource.queryItems)
+  }
+  
+}
