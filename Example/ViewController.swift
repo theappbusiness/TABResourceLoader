ff95ec0f1a80e9f4173a30c787c0451a32e0d76d
Luciano Marisi <luciano@techbrewers.com>
2016-11-14 10:23:36 +0000

    Added example for image loading

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -11,22 +11,44 @@ import TABResourceLoader
 
 class ViewController: UIViewController {
 
+  @IBOutlet private var imageView: UIImageView!
+  
   private let operationQueue = OperationQueue()
-  private let service = NetworkDataResourceService<CitiesResource>()
-
+  private let citiesService = NetworkDataResourceService<CitiesResource>()
+  private let imageService = NetworkDataResourceService<NetworkImageResource>()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    fetchJSONExample()
+    fetchImageExample()
+  }
+  
+  func fetchJSONExample() {
     let americaResource = CitiesResource(continent: "america")
-    service.fetch(resource: americaResource) { (result) in
+    citiesService.fetch(resource: americaResource) { (result) in
       print(result)
     }
-
-    let resourceOperation = ResourceOperation<NetworkDataResourceService<CitiesResource>>(resource: americaResource) { operation, result in
+    
+    let citiesResourceOperation = ResourceOperation<NetworkDataResourceService<CitiesResource>>(resource: americaResource) { operation, result in
       print(result)
     }
-    operationQueue.addOperation(resourceOperation)
-
+    operationQueue.addOperation(citiesResourceOperation)
+  }
+  
+  func fetchImageExample() {
+    let imageURL = URL(string: "http://www.theappbusiness.com/apple-touch-icon-180x180.png")!
+    let imageResource = NetworkImageResource(url: imageURL)
+    
+    imageService.fetch(resource: imageResource) { (result) in
+      // Do something with result
+    }
+    
+    let imageOperation = ResourceOperation<NetworkDataResourceService<NetworkImageResource>>(resource: imageResource) { [weak self] _, result in
+      if case let .success(image) = result {
+        self?.imageView.image = image
+      }
+    }
+    operationQueue.addOperation(imageOperation)
   }
 
 }
